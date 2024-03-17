8bb1a508ec24d754d9e64dc95910fb35a91689ae
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-03-17 19:44:07 +0000

    Add lockboxes solution

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+'''An implementation of the lockboxes problem is provided in this module
+'''
+
+def canUnlockAll(boxes):
+    """A function to determine if all boxes can be opened. boxes is a list of lists,
+    where each list represents a box. Return True if all boxes can be openend,
+    False otherwise
+    """
+
+    all_keys = set([*range(1, len(boxes))])
+    found_keys = set()
+
+    for i in range(len(boxes)):
+        if found_keys >= all_keys:
+            return True
+        if i > 0 and i not in found_keys:
+            return False
+        for key in boxes[i]:
+            found_keys.update({i, key})
+            if found_keys >= all_keys:
+                return True
+            for item in boxes[key]:
+                found_keys.add(item)
+                if found_keys >= all_keys:
+                    return True
+                
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,3 @@
+This directory files addresses the lockboxes problem: Given *n* number of lockboxes where each box is numbered
+sequentially from 0 to *n-1*, and each box may have the keys to other boxes. The goal is to write a program to
+determine if all the boxes can be opened.
