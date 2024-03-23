7b6e2293eea48027fcc7a52df074f49673e27dd0
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-03-23 15:13:48 +0000

    Add min-operations folder and answer file

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Implementation of solution for the minimum operations problem.'''
+
+
+from typing import Any, List
+from math import sqrt
+
+
+def minOperations(n: Any) -> int:
+    '''
+    Calculate and return the minimum number of opertions to result in n
+    characters in a file if only copy and paste operations are possible
+    '''
+    if type(n) is not int or n < 2:
+        return 0
+
+    factors: List = []
+    get_factors(n, factors)
+    return sum(factors)
+
+
+def get_factors(n: int, result: List):
+    '''return the prime factors of a given input number'''
+
+    if n < 2:
+        return result
+    root = int(sqrt(n)) + 2
+
+    for i in range(2, root):
+        if n % i == 0:
+            result.append(i)
+            return get_factors(n//i, result)
+    return result.append(n)
---
+++ 0x02-minimum_operations/README.md
@@ -0,0 +1,3 @@
+This project is on the minimum operations problem. The goal is to write a program that calculates the minimum
+number of operations required to produce n characters in a file if the file initially contains a single
+character and only copy and paste operations can be performed.
