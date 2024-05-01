fa8aaeb31e44cda20f04c1ee8c1c71694b82dd23
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-05-01 10:20:34 +0000

    Fix code error for StarWars API JS

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,15 +5,25 @@ const request = require('request');
 const filmId = process.argv[2];
 const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
 const url = `${filmsUrl}/${filmId}/`;
+const charactersArray = [];
+
+function addOne (value) {
+  charactersArray.push(value);
+}
 
 request(url, (error, response, body) => {
-  const characters = JSON.parse(body).characters;
   if (error) ;
-  characters.map(function (elem) {
-    request(elem, function (error, response, body) {
+  const characterUrls = JSON.parse(body).characters;
+  characterUrls.forEach((elem) => {
+    request(elem, (error, response, body) => {
       if (error) ;
-      console.log(JSON.parse(body).name);
+      const json = JSON.parse(body);
+      addOne({ name: json.name, created: new Date(json.created) });
     });
-    return null;
   });
 });
+
+setTimeout(() => {
+  charactersArray.sort((a, b) => a.created - b.created);
+  charactersArray.forEach((elem) => console.log(elem.name));
+}, 5000);
