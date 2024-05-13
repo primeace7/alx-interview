cec7b5a6d58e42eaaec958e99dd09072fd078b2e
Osaruonamen Jeffery O <osaruonamenjeffery@gmail.com>
2024-05-13 04:54:37 +0000

    Fix code error in island parameter problem

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -15,13 +15,13 @@ def check_boundary(grid, row, col):
     0  0  0  0
     '''
     count = 0
-    if grid[row][col + 1] == 1:
+    if col != len(grid[row]) - 1 and grid[row][col + 1] == 1:
         count += 1
-    if grid[row][col - 1] == 1:
+    if col != 0 and grid[row][col - 1] == 1:
         count += 1
-    if grid[row + 1][col] == 1:
+    if row != len(grid) - 1 and grid[row + 1][col] == 1:
         count += 1
-    if grid[row - 1][col] == 1:
+    if row != 0 and grid[row - 1][col] == 1:
         count += 1
 
     return count
@@ -38,24 +38,19 @@ def island_perimeter(grid):
     check = [type(row) is list for row in grid]
     if False in check:
         return 0
-    elif 1 in grid[0] or 1 in grid[-1]:
-        return 0
     elif gridsize > 100:
         return 0
     elif len(grid[0]) > 100 or len(grid[-1]) > 100:
         return 0
 
-    for row in range(1, gridsize - 1):
-        if grid[row][0] == 1 or grid[row][-1] == 1:
-            return 0
-
-        for col in range(1, len(grid[row]) - 1):
+    for row in range(0, gridsize):
+        for col in range(0, len(grid[row])):
             boundary_count = check_boundary(grid, row, col)
             if grid[row][col] and boundary_count:
                 boundaries += boundary_count
                 land += 1
             elif grid[row][col]:
-                return 0
+                land += 1
 
     if land:
         return (4 * land) - boundaries
